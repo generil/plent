4366c69599521cdf452e97aeb658ac73e0173a1c
Christian Mante <mantechristian123go@gmail.com>
2017-05-09 07:44:08 +0800

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,66 @@
+var mainState = {
+    preload: function() { 
+        // This function will be executed at the beginning     
+        // That's where we load the images and sounds 
+        game.load.spritesheet('plant', 'assets/images/spriteplant.png', 128, 128, 8);
+        // game.load.image('bkg6', 'assets/images/bkg6.gif');
+    	// game.load.audio('jump', 'assets/audio/jump.wav');
+    },
+
+    create: function() { 
+        // This function is called after the preload function     
+        // Here we set up the game, display sprites, etc.  
+    	
+    	this.game.stage.backgroundColor = "#FFFFFF";
+
+	    //add and animate plant
+		this.plant = game.add.sprite(650, 200, 'plant');
+		game.physics.enable(this.plant);
+		this.plant.body.collideWorldBounds = true;
+		// this.plant.body.gravity.y = 1000;
+		hmove = 200;
+		vmove = -300;
+		jumpTimer = 0;
+
+		//animate sprite
+		this.plant.animations.add('grow', [0, 1, 2, 3, 4, 5, 6, 7], 10, true);
+		//resize visible sprite size
+		this.plant.scale.x = 1;
+		this.plant.scale.y = 1;
+
+		//resize sprite hitbox setSize(width, height, offsetX, offsetY)
+		// this.plant.body.setSize(25,90,50,15);
+
+		//start physics system
+		game.physics.startSystem(Phaser.Physics.ARCADE);
+		game.camera.follow(this.plant);
+
+		game.world.bringToTop(this.plant); 
+		game.physics.arcade.checkCollision.right = false;
+		game.physics.arcade.checkCollision.bottom = false;
+
+		// this.timer = game.time.events.loop(1500, this.addPlatforms, this);
+    
+		//show controls
+    	previewLabel = game.add.text(game.world.centerX+25, game.world.centerY-100, '『preview』', {font: '24px Roboto', fill: '#151515'});
+		previewLabel.anchor.setTo(0.5, 0.5);
+
+		//fade controls
+    	game.time.events.add(3000, function() {
+    	    game.add.tween(previewLabel).to({y: 0}, 500, Phaser.Easing.Linear.None, true);    
+    	    game.add.tween(previewLabel).to({alpha: 0}, 500, Phaser.Easing.Linear.None, true);
+    	}, this);
+    },
+ 	
+ 	update: function() {
+    	// this.plant.animations.play('grow');
+    	this.plant.animations.play('grow');
+		
+    },
+};
+
+var game = new Phaser.Game(1300, 400, Phaser.AUTO, 'game');
+// Add the 'mainState' and call it 'main'
+game.state.add('main', mainState);
+// Start the state to actually start the game
+game.state.start('main');
