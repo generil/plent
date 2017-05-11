ceb35789944dafe3bdca7627acf678dc9645b4ef
Christian Mante <mantechristian123go@gmail.com>
2017-05-11 13:23:56 +0800

    updated main.js

--- js/main.js
+++ js/main.js
@@ -2,8 +2,10 @@ var mainState = {
     preload: function() { 
         // This function will be executed at the beginning     
         // That's where we load the images and sounds 
-        game.load.spritesheet('plant', 'assets/spriteplant.png', 128, 128, 8);
-        // game.load.image('bkg6', 'assets/images/bkg6.gif');
+        game.load.spritesheet('plant', 'assets/images/spriteplant.png', 128, 128, 8);
+        // game.load.image('bkg1', 'assets/images/bkg1.jpg');
+        game.load.image('bkg3', 'assets/images/bkg3.jpg');
+
     	// game.load.audio('jump', 'assets/audio/jump.wav');
     },
 
@@ -11,10 +13,11 @@ var mainState = {
         // This function is called after the preload function     
         // Here we set up the game, display sprites, etc.  
     	
-    	this.game.stage.backgroundColor = "#FFFFFF";
+    	// this.game.stage.backgroundColor = "#FFFFFF";
+    	this.bkg = game.add.tileSprite(0, 0, 200, 800, 'bkg3');
 
 	    //add and animate plant
-		this.plant = game.add.sprite(650, 200, 'plant');
+		this.plant = game.add.sprite(650, 275, 'plant');
 		game.physics.enable(this.plant);
 		this.plant.body.collideWorldBounds = true;
 		// this.plant.body.gravity.y = 1000;
@@ -23,10 +26,10 @@ var mainState = {
 		jumpTimer = 0;
 
 		//animate sprite
-		this.plant.animations.add('grow', [0, 1, 2, 3, 4, 5, 6, 7], 10, true);
+		this.plant.animations.add('grow', [0, 1, 2, 3, 4, 5, 6, 7], 3, true);
 		//resize visible sprite size
-		this.plant.scale.x = 1;
-		this.plant.scale.y = 1;
+		this.plant.scale.x = 0.65;
+		this.plant.scale.y = 0.65;
 
 		//resize sprite hitbox setSize(width, height, offsetX, offsetY)
 		// this.plant.body.setSize(25,90,50,15);
@@ -42,7 +45,7 @@ var mainState = {
 		// this.timer = game.time.events.loop(1500, this.addPlatforms, this);
     
 		//show controls
-    	previewLabel = game.add.text(game.world.centerX+25, game.world.centerY-100, '『preview』', {font: '24px Roboto', fill: '#151515'});
+    	previewLabel = game.add.text(game.world.centerX+25, game.world.centerY-100, '『sun flower』', {font: '24px Roboto', fill: '#151515'});
 		previewLabel.anchor.setTo(0.5, 0.5);
 
 		//fade controls
@@ -53,6 +56,7 @@ var mainState = {
     },
  	
  	update: function() {
+ 		this.bkg.tilePosition.x -= 1;
     	// this.plant.animations.play('grow');
     	this.plant.animations.play('grow');
 		
